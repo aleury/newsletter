b466c00c5196d373c0647dd674352148fc1594e6
Adam Eury <adam@coherentpath.com>
2021-08-21 14:44:06 -0400

    Setup actix-web, a health check endpoint, and integration testing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::net::TcpListener;
+
+use newsletter::run;
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+
+    run(listener)?.await
 }
